fcd93a308afb5c2c55dd81070c99722c754aa320
Vladimir <justlikevova@gmail.com>
2023-03-16 18:41:21 +0300

    fix admin

--- tgbot/handlers/admin_menu.py
+++ tgbot/handlers/admin_menu.py
@@ -7,21 +7,19 @@ from tgbot.services.data_to_json import main
 from tgbot.utils.misc.bot_filters import IsAdmin
 
 
-@dp.message_handler(text='Добавить прайс')
+@dp.message_handler(IsAdmin(), text='Добавить прайс')
 async def ask_to_send_file(message: Message):
-    if message.from_user.id in get_admins():
-        await message.answer("Пришли файл прайс листа")
+    await message.answer("Пришли файл прайс листа")
 
 
-@dp.message_handler(content_types=ContentTypes.DOCUMENT)
+@dp.message_handler(IsAdmin(), content_types=ContentTypes.DOCUMENT)
 async def get_file_from_user(message: Message):
-    if message.from_user.id in get_admins():
-        file_id = message.document.file_id
-        file = await bot.get_file(file_id)
+    file_id = message.document.file_id
+    file = await bot.get_file(file_id)
 
-        file_path = f"tgbot/data/price.txt"
-        await file.download(file_path)
-        main()
-        update_menu()
+    file_path = f"tgbot/data/price.txt"
+    await file.download(file_path)
+    main()
+    update_menu()
 
-        await message.answer("Файл успешно загружен.")
+    await message.answer("Файл успешно загружен.")
